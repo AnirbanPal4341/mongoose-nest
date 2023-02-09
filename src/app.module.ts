c5775d000cfcb5c9b4901951a1349ec051ec9a83
Anirban Pal <63857297+AnirbanPal4341@users.noreply.github.com>
2023-02-09 15:52:01 +0530

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './users/user.module';
 
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://anirban:LRvIICldBiIMAAqC@cluster0.kdwrzse.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
